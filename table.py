db9832fa14e6e427b14d522b236902b1e3791183
caitaozhan <caitaozhan@gmail.com>
2019-06-21 16:15:50 -0400

    plotting script for lines

--- table.py
+++ table.py
@@ -1,6 +1,7 @@
 
 import runner
-
+import numpy as np
+import matplotlib.pyplot as plt
 import tabulate
 
 METHODS = ['octopus-r', 'octopus-s', 'upper-bound', 'split', 'eclipse', 'octopus+']
@@ -20,7 +21,42 @@ def getMetric(inpt, output, metric = 'percent_objective_value'):
 
 	raise Exception('Unexpected metric: ' + str(metric))
 
+
+def plot_line(table, method, filename=None, x_label=None, y_label=None, legend=None):
+	'''
+	'''
+	markers    = ['o', 'h', 's', '^', 'D', 'P']     # markers
+	linestyles = ['-', '--', '-', ':', '--', '-.']  # line styles
+	plt.rcParams['font.size'] = 30
+	fig, ax = plt.subplots(figsize=(16, 15))
+	X  = [row[0] for row in table]
+	num = len(table[0]) - 1                         # number of methods
+	Y = []
+	for i in range(1, num+1):
+		y = [row[i] for row in table]
+		Y.append(y)
+	for i in range(0, num):
+		plt.plot(X, Y[i], linestyle=linestyles[i], linewidth=4, marker=markers[i], markersize=8, label=method[i])
+	
+	plt.xticks(X)
+	y_min = np.min(np.min(table, 0)[1:]) - 5
+	y_max = 100
+	plt.ylim([y_min, y_max])
+	plt.legend(bbox_to_anchor=(0., 1.), loc='lower left', ncol=3)
+
+	if x_label:
+		ax.set_xlabel(x_label)
+	if y_label:
+		ax.set_ylabel(y_label)
+	if filename:
+		fig.savefig('plot/{}.png'.format(filename))
+	else:
+		fig.savefig('plot/tmp.png')
+	#plt.show()
+
+
 if __name__ == '__main__':
+
 	data = runner.readDataFromFile('data/6-20/first_run.txt')
 
 	# Reconfig Delta test
@@ -30,5 +66,5 @@ if __name__ == '__main__':
 			rd_table[inpt.reconfig_delta] = {method: getMetric(inpt, output) for method, output in output_by_method.iteritems()}
 
 	print_table = [[rd] + [(vals_by_method[method] if method in vals_by_method else None) for method in METHODS] for rd, vals_by_method in sorted(rd_table.iteritems())]
-
 	print tabulate.tabulate(print_table, headers = ['RD'] + METHODS)
+	plot_line(print_table, METHODS, filename='vary-reconfig-delay', x_label='Reconfiguration Delay', y_label='% of Packets Delivered')
